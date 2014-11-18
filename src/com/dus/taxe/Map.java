2d1d7b1e3a564392f66e87cd4bf6723faf776806
mattcarabine <matt.carabine@hotmail.co.uk>
2014-11-18 18:02:20 +0000

    Added descriptions to all functions

--- src/com/dus/taxe/Map.java
+++ src/com/dus/taxe/Map.java
@@ -9,6 +9,7 @@ public class Map {
 
 
     public ArrayList<Node> findAdjacentNodes(Node Node1){
+        // returns a list of all nodes that have a connection with the node passed to the function
         ArrayList<Node> adjacentNodes = new ArrayList<Node>();
         for (int i=0; i<listOfNodes.size(); i++) {
             if (connections[Node1.getId()][i] != null) {
@@ -20,10 +21,12 @@ public class Map {
     }
 
     public int findDistance(Node Node1,Node Node2){
+        // returns the distance between two nodes if a connection exists, if it doesn't returns 0
         return 0;
     }
 
     public boolean checkObstruction(Node Node1,Node Node2){
+        // returns whether or not an obstruction exists between two nodes, true is so, false if no obstruction exists.
         return true;
     }
 }
