53511c88f1357a95a480c113b51d4820523f44ac
Raluca <ram534@york.ac.uk>
2014-11-18 16:03:06 +0000

    Fixed it more. Yay!

--- src/com/dus/taxe/Map.java
+++ src/com/dus/taxe/Map.java
@@ -1,9 +1,13 @@
 package com.dus.taxe;
 
+import java.util.ArrayList;
+
 public class Map {
     Connection[][] connections;
 
-    public Node[] findAdjacentNodes(Node Node1){
-        return [Node1];
+    public ArrayList<Node> findAdjacentNodes(Node Node1){
+        ArrayList<Node> adjacentNodes = new ArrayList<Node>();
+        adjacentNodes.add(Node1);
+        return adjacentNodes;
     }
 }
