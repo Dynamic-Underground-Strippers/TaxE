325ce1d93e932d09ecf091b1a611a0ef8c2e833e
mattcarabine <matt.carabine@hotmail.co.uk>
2014-11-20 15:35:00 +0000

    Updated Route Constructor

--- src/com/dus/taxe/Route.java
+++ src/com/dus/taxe/Route.java
@@ -2,13 +2,18 @@ package com.dus.taxe;
 
 import java.util.ArrayList;
 
-public class Route {
+public class Route{
     ArrayList<Node> listOfNodes;
-    int length = 0;
+    int length = 0; //Length of the route as a unit measurement
 
     public Route(ArrayList<Node> newNodes){
         setRoute(newNodes);
+        if (!isValid()){
+            throw new Error("Invalid Route");
+        }
+        length = findTotalDistance();
     }
+
 /*    public boolean isBlocked() {
         // Returns whether or not the route is blocked by an obstacle
         for (int i = 0; i < (listOfNodes.size() - 1); i++) {
@@ -18,7 +23,9 @@ public class Route {
         }
         return false;
     }*/
+
     private boolean isValid(){
+        // Checks whether a route is possible, should not be necessary as this should all be handled within the GUI
         for (int i = 0; i < (listOfNodes.size() - 1); i++) {
             if ((Game.currentMap.findDistance(listOfNodes.get(i),listOfNodes.get(i+1)))==0){
                 return false;
@@ -26,11 +33,13 @@ public class Route {
         }
         return true;
     }
+
     private void setRoute(ArrayList<Node> newNodes){
+        // Sets the listOfNodes to be equal to the list passed in the constructor
         listOfNodes = newNodes;
     }
 
-    public int findTotalDistance(){
+    private int findTotalDistance(){
         int totalDistance = 0;
         for (int i = 0; i < (listOfNodes.size() - 1); i++) {
            totalDistance += Game.currentMap.findDistance(listOfNodes.get(i),listOfNodes.get(i+1));
