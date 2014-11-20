21b870198064b813ca6f4257ed69bdbbba114729
mattcarabine <matt.carabine@hotmail.co.uk>
2014-11-20 15:25:54 +0000

    Changed nodes to listOfNodes.

--- src/com/dus/taxe/Route.java
+++ src/com/dus/taxe/Route.java
@@ -3,7 +3,7 @@ package com.dus.taxe;
 import java.util.ArrayList;
 
 public class Route {
-    ArrayList<Node> nodes;
+    ArrayList<Node> listOfNodes;
     int length = 0;
 
     public Route(ArrayList<Node> newNodes){
@@ -11,22 +11,29 @@ public class Route {
     }
 /*    public boolean isBlocked() {
         // Returns whether or not the route is blocked by an obstacle
-        for (int i = 0; i < (nodes.size() - 1); i++) {
-            if (Game.currentMap.checkObstruction(nodes.get(i),nodes.get(i+1))) {
+        for (int i = 0; i < (listOfNodes.size() - 1); i++) {
+            if (Game.currentMap.checkObstruction(listOfNodes.get(i),listOfNodes.get(i+1))) {
                 return true;
             }
         }
         return false;
     }*/
-
+    private boolean isValid(){
+        for (int i = 0; i < (listOfNodes.size() - 1); i++) {
+            if ((Game.currentMap.findDistance(listOfNodes.get(i),listOfNodes.get(i+1)))==0){
+                return false;
+            }
+        }
+        return true;
+    }
     private void setRoute(ArrayList<Node> newNodes){
-        nodes = newNodes;
+        listOfNodes = newNodes;
     }
 
     public int findTotalDistance(){
         int totalDistance = 0;
-        for (int i = 0; i < (nodes.size() - 1); i++) {
-           totalDistance += Game.currentMap.findDistance(nodes.get(i),nodes.get(i+1));
+        for (int i = 0; i < (listOfNodes.size() - 1); i++) {
+           totalDistance += Game.currentMap.findDistance(listOfNodes.get(i),listOfNodes.get(i+1));
         }
         return totalDistance;
     }
