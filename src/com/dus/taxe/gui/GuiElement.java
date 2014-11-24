d7f78b7e4f46c2bb1e08abfb7688d9d29f9697f8
Ezard <ezard.ben@gmail.com>
2014-11-24 16:46:09 +0000

    Top sections are now being used for trains instead of goals. Aforementioned sections now expand on mouse hover

--- src/com/dus/taxe/gui/GuiElement.java
+++ src/com/dus/taxe/gui/GuiElement.java
@@ -1,32 +1,108 @@
 package com.dus.taxe.gui;
 
-import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
-public abstract class GuiElement extends Rectangle {
-    private boolean mouseHover = false;
+public abstract class GuiElement extends Rect {
+	private ArrayList<GuiElement> children = new ArrayList<GuiElement>();
+	private boolean mouseHover = false;
+	private GuiElement parent;
+	private int zIndex;
 
-    public GuiElement(int x, int y, int width, int height) {
-        super(x, y, width, height);
-    }
+	public GuiElement(int x, int y, int width, int height) {
+		super(x, y, width, height);
+	}
 
-    public abstract void draw(Graphics graphics);
+	public void draw(Graphics graphics) {
+		HashMap<Integer, ArrayList<GuiElement>> map = new HashMap<Integer, ArrayList<GuiElement>>();
+		ArrayList<Integer> indices = new ArrayList<Integer>();
+		for (GuiElement child : children) {
+			if (!indices.contains(child.getZIndex())) {
+				indices.add(child.getZIndex());
+				map.put(child.getZIndex(), new ArrayList<GuiElement>());
+			}
+			map.get(child.getZIndex()).add(child);
+		}
+		Collections.sort(indices);
+		for (Integer i : indices) {
+			for (GuiElement guiElement : map.get(i)) {
+				guiElement.draw(graphics);
+			}
+		}
+	}
 
-    final void mouseMoved(MouseEvent e) {
-        if (contains(e.getPoint())) {
-            if (!mouseHover) {
-                mouseOver();
-            }
-            mouseHover = true;
-        } else {
-            if (mouseHover) {
-                mouseOut();
-            }
-            mouseHover = false;
-        }
-    }
+	public GuiElement getParent() {
+		return parent;
+	}
 
-    protected void mouseOver() {}
+	public int getZIndex() {
+		return zIndex;
+	}
 
-    protected void mouseOut() {}
+	final void mouseMoved(MouseEvent e) {
+		if (contains(e.getPoint())) {
+			if (!mouseHover) {
+				onMouseOver();
+			}
+			mouseHover = true;
+		} else {
+			if (mouseHover) {
+				onMouseOut();
+			}
+			mouseHover = false;
+		}
+		for (GuiElement child : children) {
+			child.mouseMoved(e);
+		}
+	}
+
+	public final void addChild(GuiElement child) {
+		child.setParent(this);
+		children.add(child);
+	}
+
+	protected void onMouseOver() {
+	}
+
+	protected void onMouseOut() {
+	}
+
+	public void setParent(GuiElement parent) {
+		this.parent = parent;
+	}
+
+	public GuiElement getChild(int index) {
+		return children.get(index);
+	}
+
+	public boolean contains(Point point) {
+		return contains(point.x, point.y);
+	}
+
+	public boolean contains(float x, float y) {
+		float w = width;
+		float h = height;
+		if (w < 0 || h < 0) {
+			// At least one of the dimensions is negative...
+			return false;
+		}
+		// Note: if either dimension is zero, tests below must return false...
+		float x2 = this.x + (parent == null ? 0 : parent.x);
+		float y2 = this.y + (parent == null ? 0 : parent.y);
+		if (x < x2 || y < y2) {
+			return false;
+		}
+		w += x2;
+		h += y2;
+		//    overflow || intersect
+		return ((w < x2 || w > x) && (h < y2 || h > y));
+	}
+
+	public void setZIndex(int zIndex) {
+		this.zIndex = zIndex;
+	}
 }
--- src/com/dus/taxe/gui/goals/GoalContainer.java
+++ src/com/dus/taxe/gui/goals/GoalContainer.java
@@ -6,21 +6,21 @@ import com.dus.taxe.gui.GuiElement;
 import java.awt.*;
 
 public class GoalContainer extends GuiElement {
-    private Goal[] goals = new Goal[3];
-    private final Color[] colours = {Color.RED, Color.YELLOW, Color.GREEN};
+	private GoalGuiElement[] goalGuiElements = new GoalGuiElement[3];
+	private final Color[] colours = {Color.RED, Color.YELLOW, Color.GREEN};
 
-    public GoalContainer(int x, int y, int width, int height) {
-        super(x, y, width, height);
-    }
+	public GoalContainer(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		for (int i = 0; i < goalGuiElements.length; i++) {
+			goalGuiElements[i] = new GoalGuiElement(x + (int) (i * ((float) width / 3)), y, (int) ((float) width / 3f), height);
+			goalGuiElements[i].setColour(colours[i]);
+			addChild(goalGuiElements[i]);
+		}
+	}
 
-    public void setGoals(Goal[] goals) {
-        this.goals = goals;
-    }
-
-    public void draw(Graphics graphics) {
-        for (int i = 0; i < goals.length; i++) {
-            graphics.setColor(colours[i]);
-            graphics.fillRect(x + (int) (i * ((float) width / 3)), y, (int) ((float) width / 3f), height);
-        }
-    }
+	public void setGoals(Goal[] goals) {
+		for (int i = 0; i < goalGuiElements.length; i++) {
+			goalGuiElements[i].setGoal(goals[i]);
+		}
+	}
 }
