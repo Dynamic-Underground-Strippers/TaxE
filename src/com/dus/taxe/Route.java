5becaf733d49ad99982123960302e044f0a6207c
Angelo Lupo <angelolupo94@gmail.com>
2015-01-12 15:10:51 +0000

    Comments

--- src/com/dus/taxe/Route.java
+++ src/com/dus/taxe/Route.java
@@ -58,21 +58,30 @@ public class Route{
             }
         }
     }
-
-    public void updateDistanceAlongConnection(int left){
+    //update the distance travelled along the current connection
+    //left is the distance travelled in the turn.
+    public void updateDistanceAlongConnection(int distanceTravelled){
+        //calculate the length of the connection
         int totalDistance = Game.currentMap.findDistance(this.listOfNodes.get(this.indexOfCurrentNode), this.listOfNodes.get(this.indexOfCurrentNode+1));
-        if (this.distanceAlongConnection + left < totalDistance){
-            this.distanceAlongConnection+=left;
+        //if the already travelled distance plus the just travelled distance is less than the total distance
+        //the updated travelled distance will be just the the sum of the two
+        if (this.distanceAlongConnection + distanceTravelled < totalDistance){
+            this.distanceAlongConnection+=distanceTravelled;
         }else{
+            //else the train will move to the next node in the route (but only if there one!)
             if(indexOfCurrentNode < listOfNodes.size()-1){
                 this.indexOfCurrentNode+=1;
             }else{
                 return;
             }
+            //as we are in the next node (i.e. new connection) the distance will now be zero
             this.distanceAlongConnection = 0;
-            int leftDistance = distanceAlongConnection+left-totalDistance;
+            //we need to check if we can still travel.
+            int leftDistance = distanceTravelled-totalDistance;
+            //if not we are done
             if (leftDistance == 0){
                 return;
+            //if we need to travel more we can just repeat the process
             }else{
                 updateDistanceAlongConnection(leftDistance);
             }
@@ -118,7 +127,9 @@ public class Route{
 
     private int findTotalDistance(){
         // Finds the total length of the route
+        // initialize variable
         int totalDistance = 0;
+        //adds the lenght of every connection between nodes in the route
         for (int i = 0; i < (listOfNodes.size() - 1); i++) {
            totalDistance += Game.currentMap.findDistance(listOfNodes.get(i),listOfNodes.get(i+1));
         }
