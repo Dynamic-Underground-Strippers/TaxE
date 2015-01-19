89cf4367f507f32f9fb73c94f82d76e99e65c65c
Ezard <ezard.ben@gmail.com>
2015-01-19 17:01:35 +0000

    Updated goal stuff

--- src/com/dus/taxe/gui/ButtonElement.java
+++ src/com/dus/taxe/gui/ButtonElement.java
@@ -1,49 +1,47 @@
 package com.dus.taxe.gui;
 
-import java.awt.Graphics2D;
-import java.awt.Image;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 
-import javax.swing.ImageIcon;
-
 public class ButtonElement extends GuiElement {
-	private Runnable action;
-	private Image image;
+    private Runnable action;
+    private Image image;
 
-	public ButtonElement(Rect bounds, String imageName, Runnable action) {
-		super(bounds);
-		this.action = action;
-		if (imageName != null) {
-			//noinspection ConstantConditions
-			image = new ImageIcon(getClass().getClassLoader().getResource(imageName)).getImage();
-		}
-	}
+    public ButtonElement(Rect bounds, String imageName, Runnable action) {
+        super(bounds);
+        this.action = action;
+        if (imageName != null) {
+            //noinspection ConstantConditions
+            image = new ImageIcon(getClass().getClassLoader().getResource(imageName)).getImage();
+        }
+    }
 
-	@Override
-	public void draw(Graphics2D graphics) {
-		if (image != null) {
-			graphics.drawImage(image, (int) bounds.x, (int) bounds.y, (int) bounds.width,
-					(int) bounds.height, GUI.self);
-		}
-	}
+    @Override
+    public void draw(Graphics2D graphics) {
+        if (image != null) {
+            graphics.drawImage(image, (int) bounds.x, (int) bounds.y, (int) bounds.width,
+                    (int) bounds.height, GUI.self);
+        }
+    }
 
-	@Override
-	public void mouseMoved(MouseEvent e) {
+    @Override
+    public void mouseMoved(MouseEvent e) {
 
-	}
+    }
 
-	@Override
-	public void onClick(MouseEvent e) {
-		if (action != null) action.run();
-	}
+    @Override
+    public void onClick(MouseEvent e) {
+        if (action != null) action.run();
+    }
 
-	@Override
-	public void onMouseDown(MouseEvent e) {
+    @Override
+    public void onMouseDown(MouseEvent e) {
 
-	}
+    }
 
-	@Override
-	public void onMouseUp(MouseEvent e) {
+    @Override
+    public void onMouseUp(MouseEvent e) {
 
-	}
+    }
 }
--- src/com/dus/taxe/gui/Rect.java
+++ src/com/dus/taxe/gui/Rect.java
@@ -1,70 +1,70 @@
 package com.dus.taxe.gui;
 
-import java.awt.Point;
+import java.awt.*;
 
 class Rect {
-	public float height;
-	public float width;
-	public float x;
-	public float y;
-	boolean animationRunning;
-	float animationSpeed;
-	Rect animationTargetBounds;
+    public float height;
+    public float width;
+    public float x;
+    public float y;
+    boolean animationRunning;
+    float animationSpeed;
+    Rect animationTargetBounds;
 
-	public Rect() {
-		this(0, 0, 0, 0);
-	}
+    public Rect() {
+        this(0, 0, 0, 0);
+    }
 
-	public Rect(float x, float y, float width, float height) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-	}
+    public Rect(float x, float y, float width, float height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
 
-	boolean contains(float x, float y) {
-		return x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height;
-	}
+    boolean contains(float x, float y) {
+        return x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height;
+    }
 
-	public boolean contains(Point p) {
-		return contains(p.x, p.y);
-	}
+    public boolean contains(Point p) {
+        return contains(p.x, p.y);
+    }
 
-	public boolean contains(com.dus.taxe.Point p) {
-		return contains(p.getX(), p.getY());
-	}
+    public boolean contains(com.dus.taxe.Point p) {
+        return contains(p.getX(), p.getY());
+    }
 
-	public boolean equals(Object other) {
-		if (other instanceof Rect) {
-			Rect r = (Rect) other;
-			return x == r.x && y == r.y && width == r.width && height == r.height;
-		} else {
-			return false;
-		}
-	}
+    public boolean equals(Object other) {
+        if (other instanceof Rect) {
+            Rect r = (Rect) other;
+            return x == r.x && y == r.y && width == r.width && height == r.height;
+        } else {
+            return false;
+        }
+    }
 
-	public String toString() {
-		return "Rect(x:" + x + ", y:" + y + ", width:" + width + ", height:" + height + ")";
-	}
+    public String toString() {
+        return "Rect(x:" + x + ", y:" + y + ", width:" + width + ", height:" + height + ")";
+    }
 
-	public final void update() {
-		if (animationRunning) {
-			x += (animationTargetBounds.x - x) * animationSpeed * (GUI.frameTime / 32f);
-			y += (animationTargetBounds.y - y) * animationSpeed * (GUI.frameTime / 32f);
-			width += (animationTargetBounds.width - width) * animationSpeed * (GUI.frameTime / 32f);
-			height += (animationTargetBounds.height - height) * animationSpeed *
-					(GUI.frameTime / 32f);
-			GUI.self.repaint();
-			if (Math.abs(x - animationTargetBounds.x) < 1 &&
-					Math.abs(y - animationTargetBounds.y) < 1 &&
-					Math.abs(width - animationTargetBounds.width) < 1 &&
-					Math.abs(height - animationTargetBounds.height) < 1) {
-				animationRunning = false;
-				x = animationTargetBounds.x;
-				y = animationTargetBounds.y;
-				width = animationTargetBounds.width;
-				height = animationTargetBounds.height;
-			}
-		}
-	}
+    public final void update() {
+        if (animationRunning) {
+            x += (animationTargetBounds.x - x) * animationSpeed * (GUI.frameTime / 32f);
+            y += (animationTargetBounds.y - y) * animationSpeed * (GUI.frameTime / 32f);
+            width += (animationTargetBounds.width - width) * animationSpeed * (GUI.frameTime / 32f);
+            height += (animationTargetBounds.height - height) * animationSpeed *
+                    (GUI.frameTime / 32f);
+            GUI.self.repaint();
+            if (Math.abs(x - animationTargetBounds.x) < 1 &&
+                    Math.abs(y - animationTargetBounds.y) < 1 &&
+                    Math.abs(width - animationTargetBounds.width) < 1 &&
+                    Math.abs(height - animationTargetBounds.height) < 1) {
+                animationRunning = false;
+                x = animationTargetBounds.x;
+                y = animationTargetBounds.y;
+                width = animationTargetBounds.width;
+                height = animationTargetBounds.height;
+            }
+        }
+    }
 }
