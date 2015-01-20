eb74e86975e09c58d16dcc4c09df7a52c9951a1e
Matthew Carabine <matt.carabine@hotmail.co.uk>
2015-01-20 13:50:43 +0000

    Changed route back to recursive method

--- src/com/dus/taxe/Route.java
+++ src/com/dus/taxe/Route.java
@@ -86,46 +86,45 @@ public class Route {
 		return retStr;
 	}
 
-<<<<<<< HEAD
-	public void updateDistanceAlongConnection() {
-		System.out.println(listOfNodes.size());
-		int totalDistance = Game.currentMap
-				.findDistance(this.listOfNodes.get(this.indexOfCurrentNode),
-						this.listOfNodes.get(this.indexOfCurrentNode + 1));
-		int speedPerTurn = train.getSpeed();
-		if (this.distanceAlongConnection + speedPerTurn < totalDistance) {
-			this.distanceAlongConnection += speedPerTurn;
-		} else {
-			if (indexOfCurrentNode < listOfNodes.size() - 1) {
-				this.indexOfCurrentNode += 1;
-			} else {
+
+	public void updateDistanceAlongConnection(int speed){
+		int totalDistance = Game.currentMap.findDistance(this.listOfNodes.get(this.indexOfCurrentNode), this.listOfNodes.get(this.indexOfCurrentNode+1));
+		if (this.distanceAlongConnection + speed < totalDistance){
+			this.distanceAlongConnection+=speed;
+		}else{
+			if(indexOfCurrentNode < listOfNodes.size()-1){
+				this.indexOfCurrentNode+=1;
+			}else{
 				return;
 			}
 			this.distanceAlongConnection = 0;
-			int leftDistance = distanceAlongConnection + speedPerTurn - totalDistance;
-			if (leftDistance == 0) {
+			int leftDistance = distanceAlongConnection+speed-totalDistance;
+			if (leftDistance == 0){
 				return;
-			} else {
-				updateDistanceAlongConnection(leftDistance);
-=======
-	public void updateDistanceAlongConnection(int speed) {
-		if (Game.currentMap.getConnections()[listOfNodes.get(indexOfCurrentNode).getId()][listOfNodes.get(indexOfCurrentNode+1).getId()]
-				.getDistance()-distanceAlongConnection>speed){
-			distanceAlongConnection = Game.currentMap.getConnections()[listOfNodes.get(indexOfCurrentNode).getId()][listOfNodes.get(indexOfCurrentNode+1).getId()]
-					.getDistance()-distanceAlongConnection-speed;
-		}else{
-			int remainingSpeed;
-			remainingSpeed = speed - (Game.currentMap.getConnections()[listOfNodes.get(indexOfCurrentNode).getId()][listOfNodes.get(indexOfCurrentNode+1).getId()]
-					.getDistance()-distanceAlongConnection);
-			indexOfCurrentNode++;
-			while ((remainingSpeed>Game.currentMap.getConnections()[listOfNodes.get(indexOfCurrentNode).getId()][listOfNodes.get(indexOfCurrentNode+1).getId()]
-					.getDistance()) && indexOfCurrentNode!=listOfNodes.size()){
-				remainingSpeed = remainingSpeed - (Game.currentMap.getConnections()[listOfNodes.get(indexOfCurrentNode).getId()][listOfNodes.get(indexOfCurrentNode+1).getId()].getDistance());
->>>>>>> game-methods
+			}else{
+				updateDistanceAlongConnectionRecursive(leftDistance);
 			}
-			distanceAlongConnection=remainingSpeed;
 		}
+	}
 
+	public void updateDistanceAlongConnectionRecursive(int left){
+		int totalDistance = Game.currentMap.findDistance(this.listOfNodes.get(this.indexOfCurrentNode), this.listOfNodes.get(this.indexOfCurrentNode+1));
+		if (this.distanceAlongConnection + left < totalDistance){
+			this.distanceAlongConnection+=left;
+		}else{
+			if(indexOfCurrentNode < listOfNodes.size()-1){
+				this.indexOfCurrentNode+=1;
+			}else{
+				return;
+			}
+			this.distanceAlongConnection = 0;
+			int leftDistance = distanceAlongConnection+left-totalDistance;
+			if (leftDistance == 0){
+				return;
+			}else{
+				updateDistanceAlongConnection(leftDistance);
+			}
+		}
 	}
 
 	public void useTeleport(){
